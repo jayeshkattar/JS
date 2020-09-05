3924ebe2fedc883a254166fd6d565e6d0ced03bf
Jayesh <kr.jayesh94@gmail.com>
2020-09-05 23:40:39 +0530

    DOM File changes

--- 9 - Window and DOM Manipulations/main.js
+++ 9 - Window and DOM Manipulations/main.js
@@ -1,5 +1,7 @@
+console.log(document);
+
 console.log(document.getElementById("heading1"));
 
-document.getElementById("heading1").innerHTML = "Welcome!";
+document.getElementById("heading1").innerText = "Welcome!";
 
 document.getElementById("unorderedList").innerHTML = "<li>List Item</li>";
