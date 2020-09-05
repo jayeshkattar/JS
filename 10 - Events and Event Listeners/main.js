389ef6163a2f3ba1a04ffe3a753ebb5ed44b0652
Jayesh <kr.jayesh94@gmail.com>
2020-09-05 23:56:45 +0530

    Event File changes

--- 10 - Events and Event Listeners/main.js
+++ 10 - Events and Event Listeners/main.js
@@ -1,3 +1,7 @@
 function display() {
   console.log("You clicked the Button!");
 }
+
+document.getElementById("clickButton").addEventListener("click", display);
+
+document.getElementById("clickButton").removeEventListener("click", display);
